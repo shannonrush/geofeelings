77acc8da7f61477c203614cbd52421ad8a57e9b3
Shannon Rush <shannon@shannonline.com>
2013-07-02 16:06:08 -0600

    Update tweetstream.rb

--- config/initializers/tweetstream.rb
+++ config/initializers/tweetstream.rb
@@ -1,7 +1,7 @@
 TweetStream.configure do |config|
-    config.consumer_key       = 'vAobpO62meC28RfUjEwcw'
-    config.consumer_secret    = 'Jt2IBFNv7kcIJP8BxjbuC2lcuqPo8Wfz3tgSePChBE'
+    config.consumer_key       = '...'
+    config.consumer_secret    = '...'
     config.oauth_token        = '14328274-iLO7SEMsBJlmEhKaIXCM4pGIJW5LtjZAcD3c7Iw8'
-    config.oauth_token_secret = 'lPNMr1pbsoyQvt78DZVWJHq2B05htbEaqscgsTo51HM'
+    config.oauth_token_secret = '...'
     config.auth_method        = :oauth
 end
