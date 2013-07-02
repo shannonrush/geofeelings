9ad6b282b6ce3a48203a0ac2b5f1c071db0ffa88
Shannon Rush <shannonmrush@gmail.com>
2013-07-02 12:12:20 -0600

    adding initializers

---
+++ config/initializers/tweetstream.rb
@@ -0,0 +1,7 @@
+TweetStream.configure do |config|
+    config.consumer_key       = 'vAobpO62meC28RfUjEwcw'
+    config.consumer_secret    = 'Jt2IBFNv7kcIJP8BxjbuC2lcuqPo8Wfz3tgSePChBE'
+    config.oauth_token        = '14328274-iLO7SEMsBJlmEhKaIXCM4pGIJW5LtjZAcD3c7Iw8'
+    config.oauth_token_secret = 'lPNMr1pbsoyQvt78DZVWJHq2B05htbEaqscgsTo51HM'
+    config.auth_method        = :oauth
+end
