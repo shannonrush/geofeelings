6224a58e647eee8a711a51685f4e3879100dd3c4
Shannon Rush <shannon@shannonline.com>
2013-07-04 13:07:50 -0600

    Delete twitter.rb

--- config/initializers/twitter.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-    config.consumer_key       = 'vAobpO62meC28RfUjEwcw'
-    config.consumer_secret    = '...'
-    config.oauth_token        = '14328274-iLO7SEMsBJlmEhKaIXCM4pGIJW5LtjZAcD3c7Iw8'
-    config.oauth_token_secret = '...'
-end
