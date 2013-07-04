2f97a4969fa9dad939eb228138846022f874fbcf
Shannon Rush <shannon@shannonline.com>
2013-07-04 13:08:01 -0600

    Delete tweetstream.rb

--- config/initializers/tweetstream.rb
+++
@@ -1,7 +0,0 @@
-TweetStream.configure do |config|
-    config.consumer_key       = '...'
-    config.consumer_secret    = '...'
-    config.oauth_token        = '14328274-iLO7SEMsBJlmEhKaIXCM4pGIJW5LtjZAcD3c7Iw8'
-    config.oauth_token_secret = '...'
-    config.auth_method        = :oauth
-end
